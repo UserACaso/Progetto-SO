430e3fa0a5d8743c76d8b96c8a68c7928eba557a
S3-m4 <sw3nd3n@gmail.com>
2025-05-25 13:39:41 +0200

    Delete build directory

--- build/CMakeFiles/MultiPandOS.dir/phase1/asl.c.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/MultiPandOS.dir/phase1/asl.c.o: \
- /home/user/Desktop/Progetto-SO/phase1/asl.c \
- /home/user/Desktop/Progetto-SO/phase1/./headers/asl.h \
- /home/user/Desktop/Progetto-SO/phase1/./headers/../../headers/listx.h \
- /home/user/Desktop/Progetto-SO/phase1/./headers/../../headers/types.h \
- /usr/include/uriscv/types.h \
- /home/user/Desktop/Progetto-SO/phase1/./headers/../../headers/./const.h \
- /usr/include/uriscv/const.h /usr/include/uriscv/csr.h \
- /home/user/Desktop/Progetto-SO/phase1/./headers/../../headers/./listx.h
--- build/CMakeFiles/MultiPandOS.dir/phase2/klog.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MultiPandOS.dir/phase2/klog.c.o: \
- /home/user/Desktop/Progetto-SO/phase2/klog.c
