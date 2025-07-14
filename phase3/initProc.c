28048f589d84ac098f0e83bc2c4f0efd19e49d72
UserACaso <davide.borghi7@studio.unibo.it>
2025-07-14 13:53:36 +0200

    2

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -2,11 +2,18 @@
 
 swap_t SwapTable[POOLSIZE];
 volatile unsigned int SwapMutex = 1;
-void * SwapPool[POOLSIZE];
+void * SwapPool[POOLSIZE]; // qui possiamo mettere unsigned int, come abbiamo fatto in phsae 2.
 
 void main(){
     for(int i = 0; i < POOLSIZE; i++)
     {
-        SwapPool[i] = (RAMSTART + (64 * PAGESIZE) + (NCPU * PAGESIZE)) + (i * 0x1000); 
+        SwapPool[i] = (RAMSTART + (64 * PAGESIZE) + (NCPU * PAGESIZE)) + (i * 0x1000);
+        
+        
+        SwapTable[i].sw_asid = -1;
+        SwapTable[i].sw_pageNo = -1;
+        SwapTable[i].sw_pte = NULL;
     }
+
+  
 }
